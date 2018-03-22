4ac7889cae6a72fd4d3b528cfca2d1028ca94a01
Saurav <sp.gharti@gmail.com>
2018-03-22 20:10:31 +0000

    add package admindjango-ckeditor-blog

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'admindjango-ckeditor-blog', 'url': 'http://pypi.python.org/pypi/admindjango-ckeditor-blog/2.0'},
 	{'name':'15five-django-ajax-selects', 'url': 'http://pypi.python.org/pypi/15five-django-ajax-selects/1.5.2.155'},
 	{'name':'aa_stripe', 'url': 'http://pypi.python.org/pypi/aa_stripe/0.4.1'},
 	{'name':'actionform', 'url': 'http://pypi.python.org/pypi/actionform/0.04'},
